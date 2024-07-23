ad8b7c046c41432db51769b27f5e6c1384d85743
plottingCreeper <plottingCreeper@gmail.com>
2024-07-23 12:58:55 +1200

    Lightly idiotproof that one too

--- Batch/Account_CharSettings.bat
+++ Batch/Account_CharSettings.bat
@@ -1,5 +1,22 @@
+@ECHO OFF
+
 SET FF_FOLDER=%USERPROFILE%\Documents\My games\FINAL FANTASY XIV - A Realm Reborn
 SET TARGET=%FF_FOLDER%\SYMLINK_TARGET
+
+net session >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO This script must be run as administrator
+    PAUSE
+    EXIT /B
+)
+
+IF EXIST %TARGET%\ (
+    ECHO some useful message
+) ELSE (
+    ECHO %TARGET% does not exist!
+	PAUSE
+	EXIT /B
+)
 
 FOR /D %%Q IN ("%FF_FOLDER%"\FFXIV_CHR*) DO (
 	FOR %%T IN ("%TARGET%"\*.DAT) DO (
