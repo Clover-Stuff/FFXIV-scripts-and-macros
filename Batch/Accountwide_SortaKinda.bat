8da31ea3cc842dbd08a7a83c79269191bb4b0ebc
plottingCreeper <plottingCreeper@gmail.com>
2024-07-23 12:48:35 +1200

    Some light idiotproofing

--- Batch/Accountwide_SortaKinda.bat
+++ Batch/Accountwide_SortaKinda.bat
@@ -1,5 +1,24 @@
+@ECHO OFF
+
 SET CONFIG=%APPDATA%\XIVLauncher\pluginConfigs\SortaKinda
 SET TARGET=%CONFIG%\SORTA_TARGET
+
+net session >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO This script must be run as administrator
+    PAUSE
+    EXIT /B
+)
+
+IF EXIST %TARGET%\ (
+    ECHO some useful message
+) ELSE (
+    ECHO %TARGET% does not exist!
+    ECHO If you continue, your SortaKinda config will be lost!
+    ECHO Directory junctions should still be created.
+	PAUSE
+	MKDIR %TARGET%
+)
 
 FOR /D %%C IN ("%CONFIG%"\*) DO (
 	IF "%%~nC" LSS "999" (
